f76e4472b91aae296a41160f3638ec1a0d5ea787
Taylor <twslusher@bsu.edu>
2018-09-07 17:31:47 -0400

    IntegerToNumeral.Convert now works for 1-5 test cases for 1,2,4,5 in IntegerToNumeralTest

--- Tests/IntegerToNumeralTest.java
+++ Tests/IntegerToNumeralTest.java
@@ -8,8 +8,25 @@ public class IntegerToNumeralTest{
 
     @Test
     public void TestForOne(){
-        int input = 1;
         assertEquals("I",IntegerToNumeral.Convert(1));
+
+    }
+
+    @Test
+    public void TestForTwo(){
+        assertEquals("II",IntegerToNumeral.Convert(2));
+
+    }
+
+    @Test
+    public void TestForFive(){
+        assertEquals("V",IntegerToNumeral.Convert(5));
+
+    }
+
+    @Test
+    public void TestForFour(){
+        assertEquals("IV",IntegerToNumeral.Convert(4));
 
     }
 
--- src/IntegerToNumeral.java
+++ src/IntegerToNumeral.java
@@ -2,7 +2,21 @@ public class IntegerToNumeral {
 
     public static String Convert(int input){
 
-        String output = null;
+        String output = "";
+
+        while(input > 0){
+
+            if(input >= 5) {
+                output = output + "V";
+                input = input - 5;
+            }else if(input >= 4) {
+                output = output + "IV";
+                input = input - 4;
+            }else if(input >= 1){
+                output = output + "I";
+                input = input - 1;
+            }
+        }
 
 
 
