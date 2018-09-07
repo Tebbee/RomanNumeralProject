d8d2e6e9400a561c1607aebb50120191cf3bdda5
Taylor <twslusher@bsu.edu>
2018-09-07 17:51:19 -0400

    IntegerToNumeral.Convert now works for 1-100 test cases for 1,2,4,5,9,10,14,15,38,40,50,90,100 in IntegerToNumeralTest

--- Tests/IntegerToNumeralTest.java
+++ Tests/IntegerToNumeralTest.java
@@ -30,4 +30,58 @@ public class IntegerToNumeralTest{
 
     }
 
+    @Test
+    public void TestForNine(){
+        assertEquals("IX",IntegerToNumeral.Convert(9));
+
+    }
+
+    @Test
+    public void TestForTen(){
+        assertEquals("X",IntegerToNumeral.Convert(10));
+
+    }
+
+    @Test
+    public void TestForFourteen(){
+        assertEquals("XIV",IntegerToNumeral.Convert(14));
+
+    }
+
+    @Test
+    public void TestForFifteen(){
+        assertEquals("XV",IntegerToNumeral.Convert(15));
+
+    }
+
+    @Test
+    public void TestForThirtyEight(){
+        assertEquals("XXXVIII",IntegerToNumeral.Convert(38));
+
+    }
+
+    @Test
+    public void TestForFourty(){
+        assertEquals("XL",IntegerToNumeral.Convert(40));
+
+    }
+
+    @Test
+    public void TestForFifty(){
+        assertEquals("L",IntegerToNumeral.Convert(50));
+
+    }
+
+    @Test
+    public void TestForNinety(){
+        assertEquals("XC",IntegerToNumeral.Convert(90));
+
+    }
+
+    @Test
+    public void TestForOneHundred(){
+        assertEquals("C",IntegerToNumeral.Convert(100));
+
+    }
+
 }
--- src/IntegerToNumeral.java
+++ src/IntegerToNumeral.java
@@ -1,12 +1,34 @@
 public class IntegerToNumeral {
 
+
+//    Convert accepts an integer as input and then
+//    translates to numeral
+
     public static String Convert(int input){
 
         String output = "";
 
         while(input > 0){
 
-            if(input >= 5) {
+            if(input >= 100) {
+                output = output + "C";
+                input = input - 100;
+            }else if(input >= 90) {
+                output = output + "XC";
+                input = input - 90;
+            }else if(input >= 50) {
+                output = output + "L";
+                input = input - 50;
+            }else if(input >= 40) {
+                output = output + "XL";
+                input = input - 40;
+            }else if(input >= 10) {
+                output = output + "X";
+                input = input - 10;
+            }else if(input >= 9) {
+                output = output + "IX";
+                input = input - 9;
+            }else if(input >= 5) {
                 output = output + "V";
                 input = input - 5;
             }else if(input >= 4) {
