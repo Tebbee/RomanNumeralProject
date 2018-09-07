96198a0465c2bb245893607c6c6bb2f4a6253041
Ben Rose <bcrose@bsu.edu>
2018-09-07 12:04:44 -0400

    Numeral to integer converter

---
+++ .idea/src/numeralToInteger.java
@@ -0,0 +1,15 @@
+public class numeralToInteger {
+    public final int[] letterValues = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    public final String[] numeralList = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    public int romanToInteger(String roman){
+        String error = "You must have entered something wrong, please enter a correct roman numeral";
+        for (int i = 0; i<numeralList.length; i++){
+            if(roman.startsWith(numeralList[i]))
+                return letterValues[i] + romanToInteger(roman.replaceFirst(numeralList[i], ""));
+        }
+        System.out.print(error);
+        return 0;
+    }
+    
+}
